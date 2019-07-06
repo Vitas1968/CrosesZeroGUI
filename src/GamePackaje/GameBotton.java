4aa8fbef483f64e92b43a43677e31cbb752e80b2
Vitas1968 <dor1968@mail.ru>
2019-07-06 11:29:07 +0700

    переменные, конструктор

--- src/GamePackaje/GameBotton.java
+++ src/GamePackaje/GameBotton.java
@@ -1,5 +1,16 @@
 package GamePackaje;
 
-public class GameBotton
+import javax.swing.*;
+
+public class GameBotton extends JButton
 {
+    private int buttonIndex; //номер(индекс) кнопки
+    private GameBoard board; // ссылка на игровое поле
+
+    public GameBotton(int buttonIndex, GameBoard board)
+    {
+        this.buttonIndex = buttonIndex;
+        this.board = board;
+        int rowNum = buttonIndex/ board.
+    }
 }
