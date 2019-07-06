0e328bb2197d9deb74f91437400d91384d3552ff
Vitas1968 <dor1968@mail.ru>
2019-07-06 12:03:53 +0700

    переменные,конструктор, переопредилили метод

--- src/GamePackaje/GameActionListener.java
+++ src/GamePackaje/GameActionListener.java
@@ -1,5 +1,24 @@
 package GamePackaje;
 
-public class GameActionListener
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameActionListener implements ActionListener
 {
+    private int row; //номер стоки
+    private int cell; //номер столбца
+    private GameButton button; // ссылка на кнопку
+
+    public GameActionListener(int row, int cell, GameButton button)
+    {
+        this.row = row;
+        this.cell = cell;
+        this.button = button;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
 }
