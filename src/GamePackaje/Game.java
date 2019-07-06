5bd3c6526b5fe86843443699447024c8fdf5a6e8
Vitas1968 <dor1968@mail.ru>
2019-07-06 10:36:09 +0700

    создал переменные и конструктор

--- src/GamePackaje/Game.java
+++ src/GamePackaje/Game.java
@@ -2,4 +2,12 @@ package GamePackaje;
 
 public class Game
 {
+    private GameBoard board;  // ссылка на игровое поле
+    private GamePlayers [] gamePlayers = GamePlayers [2]; //массив мгроков
+    private int playerTurn = 0; // индекс текущего игрока
+
+    public Game()
+    {
+        this.board= new GameBoard(this);
+    }
 }
