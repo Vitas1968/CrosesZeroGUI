ae355839294acd3b7a575702c0bbeb45fd716ce2
Vitas1968 <dor1968@mail.ru>
2019-07-06 10:49:45 +0700

    создан класс игрового поля

---
+++ src/GamePackaje/GameBoard.java
@@ -0,0 +1,18 @@
+package GamePackaje;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameBoard extends JFrame
+{
+    static int dimensio = 3; // размерность игрового поля
+    static int cellSize = 150; // размер клетки в пикселях
+    private char gameField [][]; // матрица игрового поля
+    private GameBotton gameBottons []; // массив кнопок
+    private  Game game; // ссылка на игру
+
+    public GameBoard(Game currentGame) throws HeadlessException
+    {
+        this.game = currentGame;
+    }
+}
