4e9bab8a8158945156d926e32b5afe5008b3576c
Vitas1968 <dor1968@mail.ru>
2019-07-06 12:17:45 +0700

    вызываем класс инитгейм и игра начинается

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,11 @@
+import GamePackaje.Game;
+
 public class MainClass
 {
     public static void main(String[] args)
     {
         Game gameInstance = new Game();
+        gameInstance.initGame();
+
     }
 }
