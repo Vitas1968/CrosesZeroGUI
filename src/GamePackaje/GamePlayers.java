d7f18a75163f3ee169b57847d49f6762ea80a04e
Vitas1968 <dor1968@mail.ru>
2019-07-06 11:08:30 +0700

    созданы переменные экземпляра, конструктор и геттеры

--- src/GamePackaje/GamePlayers.java
+++ src/GamePackaje/GamePlayers.java
@@ -2,4 +2,22 @@ package GamePackaje;
 
 public class GamePlayers
 {
+    private char playerSign;; // симол игрова крестик или нолик
+    private boolean realPlayer=true; // призак игрок это или компьютер
+
+    public GamePlayers(char playerSign, boolean realPlayer)
+    {
+        this.playerSign = playerSign;
+        this.realPlayer = realPlayer;
+    }
+
+    public char getPlayerSign()
+    {
+        return playerSign;
+    }
+
+    public boolean isRealPlayer()
+    {
+        return realPlayer;
+    }
 }
