256957d84350174cd6816ca14feae6063bf21193
Vitas1968 <dor1968@mail.ru>
2019-07-06 11:11:40 +0700

    Revert "добавлен принт"
    This reverts commit 1aa11b4311234debd953a7eb92cdaf5d1a348b48.

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,7 @@
-import GamePackaje.Game;
-
 public class MainClass
 {
     public static void main(String[] args)
     {
         Game gameInstance = new Game();
-
-        System.out.println();
     }
 }
