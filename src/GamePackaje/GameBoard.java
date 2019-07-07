e522cf030587b5b0eeb8bb5e77d771bd55743579
Vitas1968 <dor1968@mail.ru>
2019-07-07 13:22:54 +0700

    добавлен метод очистки игрового поля

--- src/GamePackaje/GameBoard.java
+++ src/GamePackaje/GameBoard.java
@@ -9,9 +9,10 @@ public class GameBoard extends JFrame
 {
     static int dimension = 3; // размерность игрового поля
     static int cellSize = 150; // размер клетки в пикселях
-    private char gameField [][]; // матрица игрового поля
-    private GameButton gameBottons []; // массив кнопок
-    private  Game game; // ссылка на игру
+    private char gameField[][]; // матрица игрового поля
+    private GameButton gameBottons[]; // массив кнопок
+    private Game game; // ссылка на игру
+    static  char nullSymbol = '\u0000'; //пустой символ
 
     public GameBoard(Game currentGame) throws HeadlessException
     {
@@ -20,13 +21,13 @@ public class GameBoard extends JFrame
     }
 
     // инциализация(отрисовка) игрового поля
-    private void  initField()
+    private void initField()
     {//основные настройки окна игры
-      setBounds(cellSize*dimension, cellSize*dimension, 400,300);
-      setTitle("Крестики-нолики");
-      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-      setLayout(new BorderLayout());
-      // панель управления игрой
+        setBounds(cellSize * dimension, cellSize * dimension, 400, 300);
+        setTitle("Крестики-нолики");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        // панель управления игрой
         JOptionPane controPanel = new JOptionPane();
         //кнопка - Новая игра
         JButton newGameButton = new JButton("Новая игра");
@@ -43,22 +44,22 @@ public class GameBoard extends JFrame
 
         controPanel.setLayout(new BoxLayout(controPanel, BoxLayout.Y_AXIS));
         controPanel.add(newGameButton);
-        controPanel.setSize(cellSize*dimension,150);
+        controPanel.setSize(cellSize * dimension, 150);
 
         // панель игры
         JPanel gameFieldPanel = new JPanel();
-        gameFieldPanel.setLayout(new GridLayout(dimension,dimension));
-        gameFieldPanel.setSize(cellSize*dimension,cellSize*dimension);
+        gameFieldPanel.setLayout(new GridLayout(dimension, dimension));
+        gameFieldPanel.setSize(cellSize * dimension, cellSize * dimension);
 
         //инициализация матрицы/массива игры
-        gameField=new char[dimension][dimension];
-        gameBottons = new GameButton[dimension*dimension];
+        gameField = new char[dimension][dimension];
+        gameBottons = new GameButton[dimension * dimension];
 
-        for (int i = 0; i < (dimension*dimension) ; i++)
+        for (int i = 0; i < (dimension * dimension); i++)
         {
             GameButton fieldButton = new GameButton(i, this);
             gameFieldPanel.add(fieldButton);
-            gameBottons [i]=fieldButton;
+            gameBottons[i] = fieldButton;
         }
 
         //добавляем панели на главную панель
@@ -66,9 +67,24 @@ public class GameBoard extends JFrame
         getContentPane().add(gameFieldPanel, BorderLayout.CENTER);
         setVisible(true);
 
-        //метод очистки игрового поля
-
     }
 
+    //метод очистки игрового поля
+    void emptyField()
+    {
+        for (int i = 0; i < (dimension * dimension); i++)
+        {
+            gameBottons[i].setText("");
+            //вычисляем номер строки
+            int x = i / GameBoard.dimension;
+            //вычисляем номер столбца
+            int y = i % GameBoard.dimension;
+            gameField [x][y]= nullSymbol;
 
+        }
+
+    }
 }
+
+
+
